64c711913ca35c64cd6f0b2622872a5e60052040
srinu <srinivasveligethi@gmail.com>
2021-05-16 17:50:50 +0530

    Completed Modal Overlay.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -6,7 +6,22 @@ import StickyHeader from './modules/StickyHeader'
 let stickyHeader=new StickyHeader()
 new RevealOnScroll(document.querySelectorAll(".feature-item"),75)
 new RevealOnScroll(document.querySelectorAll(".testimonial"),60)
-let mobilMenu=new MobileMenu();
+new MobileMenu();
 if(module.hot){
     module.hot.accept()
 }
+let modal
+document.querySelectorAll(".open-modal").forEach(el =>{
+    el.addEventListener("click",e => {
+        e.preventDefault()
+        if (typeof modal=="undefined"){
+            import('./modules/Modal').then(x => {
+            modal=new x.default()
+            setTimeout(() => modal.openTheModal(),20)
+        }).catch(() => console.log("There was a problem."))
+        }else{
+            modal.openTheModal()
+
+        }
+    })
+})
